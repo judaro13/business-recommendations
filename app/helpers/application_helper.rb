85bb2430269ebbdfe1142452e98798138be2e819
Juliana Davila <judaro13@gmail.com>
2015-04-07 20:40:08 -0500

    updated evaluator results

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,49 @@ module ApplicationHelper
     business
   end
 
+  def get_ir_recommender_result(result=nil)
+    result = JSON.parse('{
+   "took": 2,
+   "timed_out": false,
+   "_shards": {
+      "total": 5,
+      "successful": 5,
+      "failed": 0
+   },
+   "hits": {
+      "total": 1,
+      "max_score": 1,
+      "hits": [
+         {
+            "_index": "yelp_recommend",
+            "_type": "sopas",
+            "_id": "AUyWhSBC6HnAo5jpHWyZ",
+            "_score": 1,
+            "_source": {
+               "evaluation": {
+                  "precision": 0.028228831725616296,
+                  "recall": 0.04523623074915167,
+                  "f1": 0.03476389736025976,
+                  "target": {
+                     "dataUsed": "100%"
+                  }
+               },
+               "@timestamp": "2015-04-08T00:54:09.216Z",
+               "evaluator_id": null,
+               "report_type": "user_based",
+               "config": {
+                  "training_percentage": 0.9,
+                  "evaluation_percentage": 0.1,
+                  "margin_for_error": 0.5
+               }
+            }
+         }
+      ]
+   }
+}')
+  result['hits']['hits'].first['_source']['evaluation']
+  end
+
   def get_rsme_recomender_result(result=nil)
     result = JSON.parse('{
      "took": 1,
