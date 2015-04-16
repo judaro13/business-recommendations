90dac01dd3b2b007ccd1cdf63ed2d93532841cdf
Juliana Davila <judaro13@gmail.com>
2015-04-16 17:08:03 -0500

    update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,16 +4,33 @@ module ApplicationHelper
     business = []
     if recommender['_source']
       recommender['_source']['items'].each do |val|
-        business << [val['value'], 
-          Business.where(yid: val['item']['system_id']).first]
+        b = Business.where(yid: val['item']['system_id']).first
+        unless b
+          b = find_business(val['item']['system_id'])['hits']['hits'].first['_source']
+        end
+        business << [val['value'], b]
       end
     else
       recommender['hits']['hits'].first['_source']['items'].each do |val|
-        business << [val['value'], 
-          Business.where(yid: val['item']['system_id']).first]
+        b = Business.where(yid: val['item']['system_id']).first
+        business << [val['value'], b]
       end
     end
     business
+  end
+
+  def find_business(id)
+    url = "http://maps.sibcolombia.net:9200/yelp/business/_search"
+    uri =  URI.parse(url)
+    headers = {'Content-Type' => "application/json",  
+                         'Accept-Encoding'=> "gzip,deflate",
+                         'Accept' => "application/json"}
+    data = { "query": { "match": { "business_id": id } } }
+
+    http = Net::HTTP.new(uri.host,uri.port)
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    response = http.post(uri.path,data.to_json,headers)
+    JSON.parse(response.body)
   end
 
   def select_similarity
--- lib/tasks/users.rake
+++ lib/tasks/users.rake
@@ -19,21 +19,22 @@ namespace :users do
     f = File.open("/tmp/business.json", 'r+')
     f.each_line do |line|
       l = JSON.parse(line)
-      if b = Business.find_by_yid(l['business_id'])
-        # b.name = l['name']
-        # b.neighborhoods = l['neighborhoods'].join(',')
-        # b.full_address = l['full_address']
-        # b.city = l['city']
-        # b.state = l['state']
-        # b.review_count = l['review_count']
-        # b.categories = l['categories'].join(',')
-        # b.open = l['open']
-        # b.hours = l['hours'].to_s
-        # b.yattributes = l['attributes'].to_s
-        # b.stars = l['stars']
+      if !Business.find_by_yid(l['business_id'])
+        b = Business.new
+        b.name = l['name']
+        b.neighborhoods = l['neighborhoods'].join(',')
+        b.full_address = l['full_address']
+        b.city = l['city']
+        b.state = l['state']
+        b.review_count = l['review_count']
+        b.categories = l['categories'].join(',')
+        b.open = l['open']
+        b.hours = l['hours'].to_s
+        b.yattributes = l['attributes'].to_s
+        b.stars = l['stars']
         b.longitude = l['longitude']
         b.latitude = l['latitude']
-        b.save
+        b.save(validate: false)
         print '.'
       else
         print '-'
